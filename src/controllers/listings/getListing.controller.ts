59a38904b0518a9cec951d24a32855f8a31b238c
Rick Meima <rickmeima@spyce.ca>
2025-10-06 10:42:08 -0700

    changes

--- src/controllers/listings/getListing.controller.ts
+++ src/controllers/listings/getListing.controller.ts
@@ -1,17 +1,25 @@
 import { Request, Response } from "express";
 import { REPLIERS_SERVICE } from "../../services/repliers.service";
+import { AxiosError } from "axios";
 
 export const getListingController = async (
   request: Request,
   response: Response
 ) => {
-  const { mlsNumber } = request.params;
+  try {
+    const { mlsNumber } = request.params;
 
-  console.log("Fetching listing with mlsNumber", mlsNumber);
+    console.log("Fetching listing with mlsNumber", mlsNumber);
 
-  const result = await REPLIERS_SERVICE().getListing({
-    mlsNumber: mlsNumber as string,
-  });
+    const result = await REPLIERS_SERVICE().getListing({
+      mlsNumber: mlsNumber as string,
+    });
 
-  return response.json({ result });
+    return response.json({ result });
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      console.log(error.response?.data);
+    }
+    return response.status(400).json({ error: "Invalid request body" });
+  }
 };
--- src/controllers/listings/getSimilarListings.controller.ts
+++ src/controllers/listings/getSimilarListings.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { REPLIERS_SERVICE } from "../../services/repliers.service";
+import { AxiosError } from "axios";
 
 export const getSimilarListingsController = async (
   request: Request,
@@ -8,14 +9,15 @@ export const getSimilarListingsController = async (
   try {
     const { mlsNumber } = request.params;
 
-    console.log("Fetching similar listings with mlsNumber");
     const result = await REPLIERS_SERVICE().getSimilarListings({
       mlsNumber: mlsNumber as string,
     });
 
-    console.log("Result fetched", JSON.stringify(result));
     return response.json({ result });
   } catch (error) {
+    if (error instanceof AxiosError) {
+      console.log(error.response?.data);
+    }
     console.log(error);
     return response.status(400).json({ error: "Invalid request body" });
   }
